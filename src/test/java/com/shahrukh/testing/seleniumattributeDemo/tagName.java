e8a8b65448800b81378f34037f4ab1e2122c8a8d
shahrukk <shahrukh.c.khan@capgemini.com>
2020-03-26 13:04:45 +0530

    forth commit

--- src/test/java/com/shahrukh/testing/seleniumattributeDemo/tagName.java
+++ src/test/java/com/shahrukh/testing/seleniumattributeDemo/tagName.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -28,13 +30,19 @@ public class tagName {
 		String cssSelector = driver.findElement(By.cssSelector("input#email")).getCssValue("color");
 		String linkName = driver.findElement(By.linkText("Log In")).getCssValue("background-color");
 		String partialLink = driver.findElement(By.partialLinkText("Log")).getTagName();
+		Dimension size = driver.findElement(By.id("email")).getSize();
+		Point location = driver.findElement(By.id("email")).getLocation();
 		String title = driver.getTitle();
+		int titleLenght = title.length();
         System.out.println("The tag name of this field :"+tagName);
         System.out.println("The font size of this field :"+cssValue);
         System.out.println(title);
-        System.out.println("The color of this field" +cssSelector);
+        System.out.println("The title lenght :" +titleLenght);
+        System.out.println("The color of this field :" +cssSelector);
         System.out.println("The background color :" +linkName);
-        System.out.println("The tag namee :" +partialLink);
+        System.out.println("The tag name :" +partialLink);
+        System.out.println("The size :"+size);
+        System.out.println("The location :" +location);
 	}
 	
 	private void sleepNsec(int i) {
